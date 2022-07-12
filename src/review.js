230a83e9161d9e5f79a297564c211b6c37346195
mohamedfazilk <2114921@cognizant.com>
2022-07-12 23:22:00 +0530

    using index as state values and set index as zero

--- src/review.js
+++ src/review.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const review =()=> {
+  const[index, setIndex]= useState(0);
   return (
     <div className='title'><h2>review</h2></div>
   )
