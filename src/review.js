c113f573539103129ff6a8a69ad2566d8defdce4
mohamedfazilk <2114921@cognizant.com>
2022-07-13 10:55:11 +0530

    Adding the functionality for left and right buttons

--- src/review.js
+++ src/review.js
@@ -5,6 +5,23 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
  const [index, setIndex] = useState(0);
  const {name,image,job,text} = people[index];
+
+ const nextPerson = () =>{
+   setIndex((index)=>{
+    let newIndex = index + 1
+    return newIndex
+   })
+ }
+
+ const prevPerson = () =>{
+    setIndex ((index) =>{
+     let newIndex = index - 1;
+     return newIndex 
+    });}
+   
+
+
+ 
   return<article className='review'>
     <div className="img-container">
       <img src={image} alt={name} className='person-img'/>
@@ -16,8 +33,8 @@ const Review = () => {
     <p className='job'>{job}</p>
     <p className='info'>{text}</p>
     <div className="button-container">
-    <button className='prev-btn'><FaChevronLeft/></button>
-    <button className='next-btn'><FaChevronRight/></button>
+    <button onClick={prevPerson} className='prev-btn'><FaChevronLeft/></button>
+    <button onClick={nextPerson} className='next-btn'><FaChevronRight/></button>
     </div>
  
     <button className='random-btn'>surprise me</button>
